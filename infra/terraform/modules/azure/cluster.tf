aa683f0ff2ed0ae8bdcc7cbbc71b2f43873557d6
Utkarsh Srivastava <utkarsh.srivastava9295@gmail.com>
2026-06-13 19:57:25 +0530

    added helm

--- infra/terraform/modules/azure/cluster.tf
+++ infra/terraform/modules/azure/cluster.tf
@@ -21,3 +21,18 @@ resource "azurerm_log_analytics_workspace" "name" {
   location = var.location
   resource_group_name = azurerm_resource_group.myrg.name
 }
+
+
+resource "azurerm_container_registry" "name" {
+  name = "myexampleapp"
+  location = var.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  sku = "Basic"
+}
+
+
+resource "azurerm_role_assignment" "name" {
+  scope = azurerm_container_registry.name.id
+  role_definition_name = "AcrPull"
+  principal_id = azurerm_kubernetes_cluster.name.kubelet_identity[0].object_id
+}
